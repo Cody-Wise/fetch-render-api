c7ee681400a87c4a405a2a7de8091ef0af06e1f7
Cody-Wise <cody@sharptackmedia.com>
2022-03-03 12:57:10 -0800

    Updated app.js and Index page to add star-wars and audios

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 // import functions
 
-import { getPokedex } from "./fetch-utils";
+import { getPokedex, getStarWarsPeople } from './fetch-utils.js';
 
 // grab DOM elements
 
 const template = document.querySelector('#template');
-const  selectEl = document.querySelector('select');
+const selectEl = document.querySelector('select');
 
 const list = document.querySelector('#list');
+const errorElement = document.querySelector('#error-message');
+const audio = document.querySelector('audio');
+const offButton = document.querySelector('#stop');
 
 async function loadPokedex() {
     const pokedex = await getPokedex();
@@ -15,26 +18,85 @@ async function loadPokedex() {
     list.classList.add('pokemon');
 
     for (let pokemon of pokedex) {
-        const clone = template.textContent,cloneNode(true);
+        const clone = template.content.cloneNode(true);
 
-        const name = clone.querySelector('h2')
-        const image = clone.querySelector('img')
+        const name = clone.querySelector('h2');
+        const image = clone.querySelector('img');
         const type = clone.querySelector('h6');
+        audio.play();
 
         name.textContent = 'Name: ' + pokemon.pokemon;
 
         type.textContent = 'Type: ' + pokemon.type_1;
 
-        image.sre = pokemon.url_image;
+        image.src = pokemon.url_image;
         image.alt = pokemon.pokemon;
 
         list.appendChild(clone);
     }
 }
 
+async function loadStarWars() {
+    
+    const starWars = await getStarWarsPeople();
+    
+    list.classList.add('star-wars');
+   
+    for (let person of starWars) {
+        
+        const clone = template.content.cloneNode(true);
+       
+        const name = clone.querySelector('h2');
+    
+        const type = clone.querySelector('h6');
+        audio.play();
+       
+        name.textContent = ' Name: ' + person.name;
+  
+        type.textContent = ' Hair color: ' + person.hair_color;
+      
+        /// 6. Append the clone to the list element
+        list.appendChild(clone);
+      
+    
+    }
+}
+
 loadPokedex();
 
 // set event listeners 
+selectEl.addEventListener('change', async (event) => {
+    const selected = event.target.value;
+
+    if (selected === 'none'){
+        list.innerHTML = '';
+        const p = document.createElement('p');
+        p.textContent = 'please select and API';
+
+        errorElement.appendChild(p);
+    } else if (selected === 'pokemon') {
+        audio.src = './assets/pokemon.wav';
+    
+        list.innerHTML = '';
+        errorElement.innerHTML = '';
+
+        await loadPokedex();
+
+    } else if (selected === 'star-wars') {
+        audio.src = './assets/star-wars.wav';
+       
+    
+              
+        list.innerHTML = '';
+        errorElement.innerHTML = '';
+        await loadStarWars()
+    }
+
+});
+
+offButton.addEventListener('click', () => { 
+    audio.pause();
+});
     // get user input
     // use user input to update state 
     // update DOM to reflect the new state
--- fetch-utils.js
+++ fetch-utils.js
@@ -1,17 +1,17 @@
 export async function getPokedex() {
     let url = 'https://pokedex-alchemy.herokuapp.com/api/pokedex';
 
-    const response = await fetch(url)
+    const response = await fetch(url);
 
-    const jsonResponse = await response.json()
+    const jsonResponse = await response.json();
 
-    return json.results;
+    return jsonResponse.results;
 }
 
 export async function getStarWarsPeople() {
     let url = 'https://swapi.dev/api/people';
 
-    const resp = await fetch(url);
-    const json = await resp.json();
-    return json.results;
+    const response = await fetch(url);
+    const jsonResponse = await response.json();
+    return jsonResponse.results;
 }

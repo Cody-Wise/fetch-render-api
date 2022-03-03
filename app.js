2735ce06c7ea709e1f43c46e42a2366da93ea094
Cody-Wise <cody@sharptackmedia.com>
2022-03-03 11:55:39 -0800

    added loadpokedex function

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // import functions
 
+import { getPokedex } from "./fetch-utils";
+
 // grab DOM elements
 
 const template = document.querySelector('#template');
@@ -7,9 +9,30 @@ const  selectEl = document.querySelector('select');
 
 const list = document.querySelector('#list');
 
+async function loadPokedex() {
+    const pokedex = await getPokedex();
 
+    list.classList.add('pokemon');
 
-console.log(template);
+    for (let pokemon of pokedex) {
+        const clone = template.textContent,cloneNode(true);
+
+        const name = clone.querySelector('h2')
+        const image = clone.querySelector('img')
+        const type = clone.querySelector('h6');
+
+        name.textContent = 'Name: ' + pokemon.pokemon;
+
+        type.textContent = 'Type: ' + pokemon.type_1;
+
+        image.sre = pokemon.url_image;
+        image.alt = pokemon.pokemon;
+
+        list.appendChild(clone);
+    }
+}
+
+loadPokedex();
 
 // set event listeners 
     // get user input
